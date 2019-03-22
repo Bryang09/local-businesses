09be089832e99059caf266a8af27aca8b724a514
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-03-21 19:14:20 -0500

    Updates

---
+++ precache-manifest.a80927c178528842fbd8341b763de476.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7419fbf8959cccda24531931cdcb45c7",
+    "url": "/local-businesses/static/media/ipad.7419fbf8.svg"
+  },
+  {
+    "revision": "85a7efd0b879348b42ab738a790259a3",
+    "url": "/local-businesses/static/media/bg.85a7efd0.svg"
+  },
+  {
+    "revision": "53bb8f83dd4abaa38e8d4ec3d3cb9a84",
+    "url": "/local-businesses/static/media/bg.53bb8f83.svg"
+  },
+  {
+    "revision": "65062b4028546c96f4b1a6f306c8777a",
+    "url": "/local-businesses/static/media/1192.65062b40.svg"
+  },
+  {
+    "revision": "635a5aff8321df517ca2",
+    "url": "/local-businesses/static/js/runtime~main.5143fde0.js"
+  },
+  {
+    "revision": "7bb7abbe4dab844bca31",
+    "url": "/local-businesses/static/js/main.7dc49f49.chunk.js"
+  },
+  {
+    "revision": "83d6c52699e246e23b77",
+    "url": "/local-businesses/static/js/2.5202b106.chunk.js"
+  },
+  {
+    "revision": "7bb7abbe4dab844bca31",
+    "url": "/local-businesses/static/css/main.53990976.chunk.css"
+  },
+  {
+    "revision": "83d6c52699e246e23b77",
+    "url": "/local-businesses/static/css/2.6f56994f.chunk.css"
+  },
+  {
+    "revision": "a6a9e063348a28b3d432b3981fa528e0",
+    "url": "/local-businesses/index.html"
+  }
+];
